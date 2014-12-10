22ae0cea738a580d835cd5ba4a6041dc479c9def
Joao Moreno <mail@joaomoreno.com>
2014-12-10 17:07:11 +0100

    node-pre-gyp

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports = require('bindings')('archive.node');
+var binary = require('node-pre-gyp');
+var path = require('path');
+var bindingPath = binary.find(path.resolve(path.join(__dirname, 'package.json')));
+module.exports = require(bindingPath);
